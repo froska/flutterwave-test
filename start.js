d0b43ac1050ad804ced41d79e9c218da5c250606
froska <dafroska@yahoo.com>
2021-01-29 13:31:55 +0100

    Heroku 503: changed port to test

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 import app from "./server.mjs";
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
 });
